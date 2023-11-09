2e8864e3862c7ad09b27322f49d5480419e916c9
kimcheawon_Cute <77391033+kimcheawoncute@users.noreply.github.com>
2023-11-09 16:57:47 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+## 크롬드라이버 자신의 크롬버전에 맞게 업뎃하세요
+[크롬드라이버 다운로드](https://chromedriver.chromium.org/downloads)
+
+## 자충시 2captcha API키 필요
+[2captcha](https://2captcha.com)
---
+++ 백업/dump.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION;
+CREATE TABLE main(
+            user TEXT,
+            user_id TEXT,
+            money TEXT,
+            black TEXT,
+            wrong_pin TEXT,
+            accumulated TEXT
+            );
+INSERT INTO "main" VALUES('kimcheawon__#0','907204815673778176','100000998','no','0','0');
+COMMIT;
